9ef8d6513334644a6c48313bb595c903672776ae
charbel sarkis <charbelsako@gmail.com>
2019-07-03 22:36:29 +0300

    made all the functions take their argument as a reference. Added 2 sentinel values one is global called sentinel that tells the program whether or not to do the algorithm again the second is an indicator for the sentinel value. where if the second sentinel is 0 then the global sentinel should be 0 too.

--- comma.cpp
+++ comma.cpp
@@ -5,10 +5,12 @@
 using namespace std;
 
 int num_words(string str);
-void add_commas(string str);
-void get_words(int i, string str);
+void add_commas(string &str);
+void get_words(int i, string &str);
 // adds a comma before or after (where) a give `word` in a `paragraph`
 void add_commas_to(string &paragraph, string word, char where);
+
+int sentinel = 1;
 
 int main(void)
 {
@@ -25,7 +27,12 @@ int main(void)
     // Number of words in the paragraph
     int len = num_words(paragraph);
 
-    add_commas(paragraph);
+    do
+    {
+        add_commas(paragraph);
+    } while (sentinel == 1);
+
+    cout << paragraph << endl;
 
     return 0;
 }
@@ -44,7 +51,7 @@ int num_words(string str)
     return len;
 }
 
-void add_commas(string paragraph)
+void add_commas(string &paragraph)
 {
     for (int i = 0, n = paragraph.length(); i < n; i++)
     {
@@ -56,7 +63,7 @@ void add_commas(string paragraph)
     }
 }
 
-void get_words(int i, string str)
+void get_words(int i, string &str)
 {
     // or store results in global array with not duplicates.
 
@@ -74,9 +81,6 @@ void get_words(int i, string str)
     }
     add_commas_to(str, prev, 'a');
 
-    // store into global array;
-    // TODO
-
     // get the next word
     int end = i + 1;
     while (str[end + 1] != ' ' && str[end + 1] != '.')
@@ -92,38 +96,35 @@ void get_words(int i, string str)
     add_commas_to(str, next, 'b');
     // store into global array;
     // TODO
-    cout << str << endl;
 }
 
 void add_commas_to(string &paragraph, string word, char where)
 {
     size_t found = paragraph.find(word);
-
+    int k = 0;
     while (found != -1 && found < paragraph.length() - word.length())
     {
-        // cout << "found " << word << " at: " << found << '\n';
-        // cout << found + word.length() << " " << paragraph.length() << endl;
         if (where == 'b')
         {
             if (paragraph[found - 2] != ',' && paragraph[found - 2] != '.')
             {
+                k = 1;
                 paragraph.insert(found - 1, ",");
-                // cout << "inserting at " << found + word.length() << endl;
             }
         }
         else if (where == 'a')
         {
             if (paragraph[found + word.length()] != ',' && paragraph[found + word.length()] != '.')
             {
+                k = 1;
                 paragraph.insert(found + word.length(), ",");
-                // cout << "inserting at " << found + word.length() << endl;
             }
         }
 
         found = paragraph.find(word, found + word.length());
-        // cout << paragraph << endl;
     }
-
-    // cout << paragraph << endl;
-    // return paragraph;
+    if (k == 0)
+    {
+        sentinel = 0;
+    }
 }
