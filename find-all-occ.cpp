d60ed66bc48bd9e3abf09958888dd01b44c48503
Charbel Sarkis <charbelsako@gmail.com>
2020-03-19 21:31:14 +0200

    find all occurences algorithm updated.

--- find-all-occ.cpp
+++ find-all-occ.cpp
@@ -1,19 +1,34 @@
+// This file includes 2 different ways of finding occurences of a single word
+
 #include <iostream>
-#include <istream>
 #include <string>
 
 using namespace std;
 
 int main()
 {
-  string s;
-  getline(cin, s);
-  auto found = s.find("hello");
-  while (found != -1)
-  {
-    cout << found << endl;
-    found = s.find("hello", found + 1);
+  // string s;
+  // getline(cin, s);
+  // auto found = s.find("hello");
+  // while (found != -1)
+  // {
+  //   cout << found << endl;
+  //   found = s.find("hello", found + 1);
+  // }
+
+  string input;
+  int i = 0;
+  int cat_occ = 0;
+
+  cout << "Please enter a line of text: ";
+  getline(cin, input, '\n');
+
+  for (int i = input.find("cat", 0); i != string::npos; i = input.find("cat", i)) {
+    cat_occ++;
+    i++;
   }
+
+  cout << "The word cat appears " << cat_occ << " in the string " << endl << '"' << input << '"' << endl;
 
   return 0;
 }
