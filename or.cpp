9274b4ab8d6456df38e0c038dcada2c6456843e4
Charbel Sarkis <syko@localhost.localdomain>
2020-03-19 19:10:58 +0200

    added bitwise operations

---
+++ or.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int x = 4;
+    printf("%d\n", 1 | 2); 
+    printf("%d\n", 1 + 2);
+}
---
+++ xor.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string variables="xy";
+    return 0;
+}
