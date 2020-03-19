ddcce1de0b54ce409d0d8338b5d27701f2ae2474
Charbel Sarkis <charbelsako@gmail.com>
2020-03-19 21:31:22 +0200

    problem A finished

---
+++ problemA.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    ifstream input;
+    input.open("or.in");
+
+    int t;
+    input >> t;
+
+    int R, L;
+    for (int i = 0; i < t; i++) {
+        int count = 0;
+        input >> L >> R;
+        for (int a = L; a <= R; a++) {
+            for (int b = L; b <= R; b++) {
+                if ((a+b) == (a|b) ){
+                    count ++;
+                }
+            }
+        } 
+        printf("%d\n", count % (int)(pow(10,9) + 7) );
+    }
+}
