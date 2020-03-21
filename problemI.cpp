f17a9ed6106ea051713e7b6cb1c92f52e97623a4
Charbel Sarkis <charbelsako@gmail.com>
2020-03-21 18:50:08 +0200

    Added files for problemI

---
+++ problemI.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    ifstream infile;
+    infile.open("input/sumgenerated.in");
+
+    int t;
+    infile >> t;
+    
+}
