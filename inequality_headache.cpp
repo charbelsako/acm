1f3b7444259106c67efe62571b510bec81799dbc
Charbel Sarkis <charbelsako@gmail.com>
2020-03-21 18:49:48 +0200

    added an instruction file on how to modfiy strings in c++

--- inequality_headache.cpp
+++ inequality_headache.cpp
@@ -1,38 +1,57 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-void permutations(int vp, string b, char va[]);
+void permutations(int vp, string b, char va[], string a, string c, string d);
+
 int count = 0;
 
 int main() {
     string a = "xy";
-    auto b = string{2, '_'};
-    printf("%s\n", b);
+    string b = "xy";
+
+    string c = "yx";
+    string d = "yx";
+
+    printf("%s\n", b.c_str());
+    
     char va[2] = {'x', 'y'};
     int vp = 0;
-    permutations(vp, b, va); 
+    
+    permutations(vp, b, va, a, c, d);
+    
     cout << count << endl;
 }
 
-void permutations(int vp, string b, char va[]){
-    for (int i = 0; i < 9; i++) {
-        auto pos = b.find(va[vp]);
+void permutations(int vp, string b, char va[], string a, string c, string d) {
+    for (int i = 1; i < 10; i++) {
+        auto pos = a.find(va[vp]);
         if(pos != std::string::npos) {
             do {   
-                b[pos] = i;   // overwrite
+                b[pos] = (char) i+48;   // overwrite
                 pos = b.find(va[vp], pos); // find next ocurrence
             } while(pos != std::string::npos);
         }
 
+        auto pos2 = c.find(va[vp]);
+        if(pos2 != std::string::npos) {
+            do {   
+                d[pos2] = (char) i+48;   // overwrite
+                pos2 = c.find(va[vp], pos2); // find next ocurrence
+            } while(pos2 != std::string::npos);
+        }
+        
         if (vp < 2) {
-            cout << "recursive" << endl;
+            // cout << "recursive" << endl;
             // a is a pointer, hence the bug
-            permutations(++vp, b, va);
+            int t_vp = vp;
+            t_vp++;
+            permutations(t_vp, b, va, a, c, d);
         } else {
             // perform inequality if
-            printf("%s\n", b);
-            count++; 
+            // printf("%s\n", b.c_str());
+            count++;
             return;
         }
     }
--- modify_strings.cpp
+++ modify_strings.cpp
@@ -6,6 +6,12 @@ using namespace std;
 int main() {
     string s = "what";
     printf("Original: %s\n", s.c_str());
+    s[0] = (char) 98;
+    s[0] =  'a';
     printf("New: %s\n", s.c_str());
+
+    string a = "xy";
+    // Other ways to perform the same thing
+
 
 }
