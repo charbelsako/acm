3fd3a62c9fcbe480ff6940852546d6c57f07678a
Charbel Sarkis <charbelsako@gmail.com>
2020-03-21 13:36:28 +0200

    Added a small test file for modifying strings

---
+++ modify_strings.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string s = "what";
+    printf("Original: %s\n", s.c_str());
+    printf("New: %s\n", s.c_str());
+
+}
